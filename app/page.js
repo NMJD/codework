868776f4204f41a7c017ef0c9364d2bd89a682b4
nmjd <spectrermn@gmail.com>
2025-02-07 13:03:02 +0100

    Polish homepage

--- app/page.js
+++ app/page.js
@@ -1,23 +1,27 @@
-import Image from "next/image";
 import ButtonLogin from "./components/ButtonLogin";
+import FAQListItem from "./components/FAQListItem";
+
 
 export default function Home() {
   const isLoggedIn = true;
   const name = 'Michel';
+  const age = 2;
   return (
     <main>
+      {/* HEADER */}
       <section className="bg-base-200">
         <div className="max-w-3xl mx-auto flex justify-between items-center px-8 py-2">
           <div className="font-bold">CodeWork</div>
           <div className="space-x-4 max-md:hidden">
-            <a href="#" className="link link-hover">Pricing</a>
-            <a href="#" className="link link-hover">FAQ</a>
+            <a href="#pricing" className="link link-hover">Pricing</a>
+            <a href="#faq" className="link link-hover">FAQ</a>
           </div>
           <div>
             <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
           </div>
         </div>
       </section>
+      {/* HERO */}
       <section className="text-center py-32 px-8 max-w-3xl mx-auto">
         <h1 className="text-4xl lg:text-5xl font-extrabold mb-6">Collect customer feedback to build better products</h1>
         <div className="opacity-90 mb-10">
@@ -26,6 +30,67 @@ export default function Home() {
         </div>
         <ButtonLogin isLoggedIn={isLoggedIn} name={name} />
       </section>
-    </main>
+      {/* PRICING */}
+      <section className="bg-base-200" id="pricing">
+        <div className="py-32 px-8 max-w-3xl mx-auto">
+          <p className="text-sm uppercase font-medium text-center text-primary mb-4">Pricing</p>
+          <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center">A pricing that adapts to your needs</h2>
+
+          <div className="p-8 bg-base-100 max-w-96 rounded-3xl mx-auto space-y-6">
+            <div className="flex gap-2 items-baseline">
+              <div className="text-4xl text-black">$19</div>
+              <div className="text-sm uppercase font-medium opacity-60">/month</div>
+            </div>
+
+            <ul className="space-y-2">
+              {
+                ["Collect customer feedback", "Unlimited boards", "Admin dashboard", "24/7 support"].map((item) => (
+                  <li className="flex items-center gap-2" key={item}>
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="text-green-600 size-4">
+                      <path fillRule="evenodd" d="M12.416 3.376a.75.75 0 0 1 .208 1.04l-5 7.5a.75.75 0 0 1-1.154.114l-3-3a.75.75 0 0 1 1.06-1.06l2.353 2.353 4.493-6.74a.75.75 0 0 1 1.04-.207Z" className="flex items-center gap-2" clipRule="evenodd" />
+                    </svg>
+                    {item}
+                  </li>)
+                )
+              }
+            </ul>
+            <ButtonLogin isLoggedIn={isLoggedIn} name={name} extraStyle='w-full' />
+
+          </div>
+
+        </div>
+      </section>
+      {/* FAQ */}
+      <section className="bg-base-200" id="faq">
+        <div className="py-32 px-8 max-w-3xl mx-auto">
+          <p className="text-sm uppercase font-medium text-center text-primary mb-4">FAQ</p>
+          <h2 className="text-3xl lg:text-4xl font-extrabold mb-12 text-center">
+            Frequently Asked Questions
+          </h2>
+
+          <ul className="max-w-lg mx-auto">
+            {
+              [
+                {
+                  question: "What can I get exactly?",
+                  answer: "loreum Ipseum",
+                },
+                {
+                  question: "Can I get a refund?",
+                  answer: "loreum Ipseum",
+                },
+                {
+                  question: "I have another question",
+                  answer: "loreum Ipseum",
+                },
+              ].map((qa) => (
+                <FAQListItem key={qa.question} qa={qa} />
+              ))
+            }
+          </ul>
+
+        </div>
+      </section >
+    </main >
   );
 }
